37e73508f4517302982beeef02d5d73d9da2f9a8
yuka120 <yuka.cherry.0926@icloud.com>
2022-04-09 17:39:14 +0900

    week1のゼミ前に保存

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -14,7 +14,27 @@ const hikakin = {
 
 // 練習問題
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
-
+hikakin.sayHello() // ×hikakin.sayHello
 // [やってみよう]
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
+const yuka = {
+  neme: "ゆかたん",
+  age: 20,
+  hobbies: ["cooking", "volley ball", "singing"],
+  isHappy: true,
+  address: {
+    country: "日本",
+    city: "福岡",
+  },
+  sayHello: function () {
+    console.log("やっほー")
+  },
+}
+
+yuka.sayHello()
+
 // オブジェクトにはたくさんのプロパティを追加してください。
+yuka.color = "yellow"
+yuka.hair = "long"
+yuka.school = "九州大学"
+console.log(yuka)
